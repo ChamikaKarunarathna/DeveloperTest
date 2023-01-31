1f51770743c804700da9140e4129e103e4ee8578
Chamika Karunarathna <cklkarunarathnavta@gmail.com>
2023-01-31 12:19:14 +0530

    check end and start time

--- php/input-data.php
+++ php/input-data.php
@@ -8,12 +8,15 @@
         $end_time = mysqli_real_escape_string($conn,$_REQUEST['end_time']);
         $laps = mysqli_real_escape_string($conn,$_REQUEST['laps']);
 
+        // Calculate Duration in hours
+        $duration = number_format(((strtotime($end_time) - strtotime($st_time))/60)/60,2);
+
         if ($radius>$max_radius) {
             echo "<script>
                     window.location.href = '../input-form.php';
                     alert('Maximum Radius Exceeded !!');
                 </script>";
-        } else {
+        } elseif ($duration > 0) {
             $inp_query = "INSERT INTO `running_data`( `name`, `radius`, `start_time`, `end_time`, `laps_count`) VALUES ('$name','$radius','$st_time','$end_time','$laps')";
             $result = mysqli_query($conn,$inp_query);
             if ($result) {
@@ -24,6 +27,12 @@
                     alert('Database ERROR !! Please contact site Admin !');
                 </script>";
             }
+        } else {
+            echo "<script>
+                    window.location.href = '../input-form.php';
+                    alert('End date cannot be before start date !!');
+                </script>";
         }
+
     }
 ?>
